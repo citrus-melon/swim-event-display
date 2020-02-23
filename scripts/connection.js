3f2328c66ac7dc63d3fefe95ac959fc49af23d48
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2020-02-23 15:44:50 -0500

    add basic reconnection

--- scripts/connection.js
+++ scripts/connection.js
@@ -8,8 +8,6 @@ if (window.location.search) {
     window.location = "citrus-melon.github.io/swim-event-display";
 }
 
-const socket = new WebSocket ('wss://websocket-room.now.sh/swimeventdisplay_'+room);
-
 const localState = {
     number: 1,
     tts: true,
@@ -18,63 +16,72 @@ const localState = {
     hideNumber: false
 }
 
-socket.sendNumber = () => {
-    let msg = {
-        type: "number",
-        value: localState.number
-    };
-    socket.send(JSON.stringify(msg));
-}
-
-socket.sendGeneric = () => {
-    let msg = {
-        type: "all",
-        value: localState
+const initializeConnection = () => {
+    socket.sendNumber = () => {
+        let msg = {
+            type: "number",
+            value: localState.number
+        };
+        socket.send(JSON.stringify(msg));
     }
-    socket.send(JSON.stringify(msg));
-}
-
-socket.requestUpdate = () => {
-    let msg = {
-        type: "request",
+    
+    socket.sendGeneric = () => {
+        let msg = {
+            type: "all",
+            value: localState
+        }
+        socket.send(JSON.stringify(msg));
     }
-    socket.send(JSON.stringify(msg));
-}
-
-socket.onmessage = (event) => {
-    if (event.data instanceof Blob && typeof playRecorded == "function") {
-        playRecorded(event);
-        return;
+    
+    socket.requestUpdate = () => {
+        let msg = {
+            type: "request",
+        }
+        socket.send(JSON.stringify(msg));
     }
-    var msg = JSON.parse(event.data);
-    switch(msg.type) {
-      case "number":
-        localState.number = msg.value;
-        numberDisplay.innerText = localState.number;
-        if (typeof backgroundAnimate == "function") backgroundAnimate();
-        break;
-      case "all":
-        localState.number = msg.value.number;
-        localState.tts = msg.value.tts;
-        localState.sfx = msg.value.sfx;
-        localState.msg = msg.value.msg;
-        localState.hideNumber = msg.value.hideNumber;
-        updateDOM();
-        break;
-      case "request":
-        socket.sendGeneric();
-        break;
+    
+    socket.onmessage = (event) => {
+        if (event.data instanceof Blob && typeof playRecorded == "function") {
+            playRecorded(event);
+            return;
+        }
+        var msg = JSON.parse(event.data);
+        switch(msg.type) {
+          case "number":
+            localState.number = msg.value;
+            numberDisplay.innerText = localState.number;
+            if (typeof backgroundAnimate == "function") backgroundAnimate();
+            break;
+          case "all":
+            localState.number = msg.value.number;
+            localState.tts = msg.value.tts;
+            localState.sfx = msg.value.sfx;
+            localState.msg = msg.value.msg;
+            localState.hideNumber = msg.value.hideNumber;
+            updateDOM();
+            break;
+          case "request":
+            socket.sendGeneric();
+            break;
+        }
+    }
+    
+    socket.onerror = () => {
+        alert("A connection error has occurred, please reload the page")
+    }
+    
+    socket.onclose = (e) => {
+        setTimeout(() => {
+            socket = new WebSocket ('wss://websocket-room.now.sh/swimeventdisplay_'+room);
+            initializeConnection();
+            console.log('reconncet!');
+        }, 100);
+    }
+    
+    socket.onopen = () => {
+        socket.requestUpdate()
     }
 }
 
-socket.onerror = (e) => {
-    alert("A connection error has occurred, please reload the page")
-}
-
-socket.onclose = (e) => {
-    alert("The server was disconnected, please reload the page\nDetails: " + e.reason)
-}
-
-socket.onopen = () => {
-    socket.requestUpdate()
-}
+let socket = new WebSocket ('wss://websocket-room.now.sh/swimeventdisplay_'+room);
+initializeConnection();
